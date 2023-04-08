fb7db61b986c59b27a40380b4b50c621a06d732c
arjun <arjun2000raj@gmail.com>
2023-04-09 04:01:02 +0530

    change code

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,7 +9,7 @@ const sendToken = (user, statusCode, res) => {
       Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
     ),
   };
-  res.status(statusCode).cookie("token", token, option).json({
+  res.cookie("token", token, option).status(statusCode).json({
     success: true,
     token,
     user,
